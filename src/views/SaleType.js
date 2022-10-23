7edade177df2797059ce876bb8c9f192360e9419
zamora441 <101614191+zamora441@users.noreply.github.com>
2022-10-23 02:25:02 -0600

    Done SaleType

--- src/views/SaleType.js
+++ src/views/SaleType.js
@@ -1,3 +1,20 @@
-const SaleType = () => {};
+import { Link } from "react-router-dom";
+import SaleTypeTable from "../components/SaleTypeTable";
+
+const SaleType = () => {
+    return (
+        <div className=" mt-5 px-5 w-50 container">
+            <SaleTypeTable />
+            <div className="d-grid gap-2 col-6 mx-auto">
+                <Link
+                    to="agregarTipo"
+                    class="btn btn-dark text-info fs-5 fw-bolder"
+                    type="button">
+                    Agregar tipo venta
+                </Link>
+            </div>
+        </div>
+    );
+};
 
 export default SaleType;
