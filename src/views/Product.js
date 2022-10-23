18d15ba8f57126950df5d27e2d460c578533ec6e
zamora441 <101614191+zamora441@users.noreply.github.com>
2022-10-23 02:26:09 -0600

    Done Product

--- src/views/Product.js
+++ src/views/Product.js
@@ -1,3 +1,20 @@
-const Product = () => {};
+import { Link } from "react-router-dom";
+import ProductTable from "../components/ProductTable";
+
+const Product = () => {
+    return (
+        <div className="container  mt-5">
+            <ProductTable />
+            <div class="d-grid gap-2 col-6 mx-auto">
+                <Link
+                    to="agregarProducto"
+                    className="btn btn-dark text-info fs-5 fw-bolder"
+                    type="button">
+                    Agregar Producto
+                </Link>
+            </div>
+        </div>
+    );
+};
 
 export default Product;
