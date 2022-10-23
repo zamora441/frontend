9b1cf808df961aa4b8e9d3c5b1f204ba387515a6
zamora441 <101614191+zamora441@users.noreply.github.com>
2022-10-23 02:25:15 -0600

    Done Sales

--- src/views/Sales.js
+++ src/views/Sales.js
@@ -1,3 +1,10 @@
-const Sales = () => {};
+import SalesTable from "../components/SalesTable";
+const Sales = () => {
+    return (
+        <div className="container  mt-5">
+            <SalesTable />
+        </div>
+    );
+};
 
 export default Sales;
